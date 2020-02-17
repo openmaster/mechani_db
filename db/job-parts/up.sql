3c000285802e574c2843260c847c07e3c1c51321
Vishavjeet Singh <vishav4@gmail.com>
2020-02-17 14:40:46 -0500

    fixed job parts

--- db/job-parts/up.sql
+++ db/job-parts/up.sql
@@ -1,5 +1,4 @@
 CREATE TABLE core.job_parts (
-  id                  BIGINT NOT NULL DEFAULT sp.global_seq_val('core.job-parts'),
   client_id           BIGINT NOT NULL REFERENCES core.clients(id),
   site_id             BIGINT NOT NULL REFERENCES core.sites(id),
   job_id              BIGINT NOT NULL REFERENCES core.jobs(id),
@@ -7,12 +6,11 @@ CREATE TABLE core.job_parts (
   quantity            SMALLINT NOT NULL,
   created             TIMESTAMP NOT NULL DEFAULT current_timestamp,
   updated             TIMESTAMP NOT NULL DEFAULT current_timestamp,
-  PRIMARY KEY (id)
+  PRIMARY KEY (client_id, site_id, job_id, part_id)
 );
  ALTER TABLE core.job_parts ADD CONSTRAINT client_site_job_id_part_id UNIQUE(client_id, site_id, job_id, part_id);
 
  CREATE FUNCTION sp.job_parts(
-   _id                    BIGINT,
    _client_id             BIGINT,
    _site_id               BIGINT,
    _job_id                BIGINT,
@@ -23,22 +21,19 @@ CREATE TABLE core.job_parts (
   _row RECORD;
   _new_id BIGINT;
 BEGIN
-  SELECT INTO _row * FROM core.job_parts WHERE id = _id;
+  SELECT INTO _row * FROM core.job_parts WHERE client_id = _client_id and site_id = _site_id and job_id = _job_id and part_id = _part_id;
   IF FOUND THEN
     IF ROW(
-      _row.job_id,
       _row.part_id,
       _row.quantity
     ) IS DISTINCT FROM (
-      _job_id,
       _part_id,
       _quantity
     ) THEN
       UPDATE core.job_parts SET
-      job_id     =   _job_id,
       part_id    =   _part_id,
       quantity   =   _quantity
-        WHERE id = _id;
+      WHERE client_id = _client_id and site_id = _site_id and job_id = _job_id and part_id = _part_id;
       RETURN 'updated';
     ELSE
       RETURN 'unchanged';
@@ -56,8 +51,8 @@ BEGIN
       _job_id,
       _part_id,
       _quantity
-    ) RETURNING id INTO _new_id;
-    RETURN _new_id::TEXT;
+    );
+    RETURN "added";
   END IF;
 END;
 $$LANGUAGE plpgsql;
